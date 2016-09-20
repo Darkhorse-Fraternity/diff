ef779758b8b38fd6624fa4d5251c152b8e95a88b
tonyYo <420156367@qq.com>
2016-09-21 03:26:02 +0800

    add blur

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -9,29 +9,49 @@ import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
+  findNodeHandle,
+  Image,
 } from 'react-native';
-
+const { BlurView, VibrancyView } = require('react-native-blur');
+const uri = 'https://d13yacurqjgara.cloudfront.net/users/632930/screenshots/2883377/__3.jpg'
 export default class Main extends Component {
 
+  constructor(props:Object){
+      super(props);
+      this.state={
+        viewRef:0,
+      }
+    }
+    state:{
+      viewRef:number
+    };
+
+  imageLoaded() {
+    this.setState({viewRef: findNodeHandle(this.refs.backgroundImage)||0})
+  }
   componentDidMount(){
 
   }
 
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
+      <Image source={{uri}}
+         ref={'backgroundImage'}
+         onLoadEnd={this.imageLoaded.bind(this)}
+        style={styles.container}>
+        {/*<BlurView blurType="light" style={styles.blur}>
+          <Text>Hi, I am a tiny menu item</Text>
+        </BlurView>*/}
+        <BlurView
+         blurRadius={10}
+         downsampleFactor={5}
+         overlayColor={'rgba(255, 255, 255, 0.1)'}
+         style={styles.blurView}
+         viewRef={this.state.viewRef}
+       />
+        <Text style={styles.welcome}>Blur component</Text>
+      </Image>
     );
   }
 }
@@ -39,18 +59,28 @@ export default class Main extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  blur: {
+    flex:1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
   },
   instructions: {
     textAlign: 'center',
     color: '#333333',
     marginBottom: 5,
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+    color: '#FFFFFF',
+  },
+  blurView: {
+    position: "absolute",
+    left: 0,
+    top: 0,
+    bottom: 0,
+    right: 0
   },
 });
