aeb7063b4e808b7bb7fc0e5c553f94d9ed8a5c69
tonyYo <420156367@qq.com>
2017-03-17 01:30:55 +0800

    安卓配置好阿里百川相关业务

--- android/app/proguard-rules.pro
+++ android/app/proguard-rules.pro
@@ -64,3 +64,19 @@
 -dontwarn java.nio.file.*
 -dontwarn org.codehaus.mojo.animal_sniffer.IgnoreJRERequirement
 -dontwarn okio.**
+
+
+-keepattributes Signature
+-keep class sun.misc.Unsafe { *; }
+-keep class com.taobao.* {*;}
+-keep class com.alibaba.** {*;}
+-keep class com.alipay.** {*;}
+-dontwarn com.taobao.**
+-dontwarn com.alibaba.**
+-dontwarn com.alipay.**
+-keep class com.ut.** {*;}
+-dontwarn com.ut.**
+-keep class com.ta.** {*;}
+-dontwarn com.ta.**
+-keep class org.json.** {*;}
+-keep class com.ali.auth.** {*;}
--- android/app/src/main/java/com/diff/MainApplication.java
+++ android/app/src/main/java/com/diff/MainApplication.java
@@ -1,10 +1,16 @@
 package com.diff;
 
 import android.app.Application;
+import android.util.Log;
 
 import com.aakashns.reactnativedialogs.ReactNativeDialogsPackage;
+import com.alibaba.baichuan.android.trade.AlibcTradeSDK;
+import com.alibaba.baichuan.android.trade.callback.AlibcTradeInitCallback;
+import com.alibaba.baichuan.android.trade.model.AlibcTaokeParams;
 import com.cmcewen.blurview.BlurViewPackage;
 import com.facebook.react.ReactApplication;
+import com.facebook.soloader.SoLoader;
+import com.react.taobaobaichuanapi.BaiChuanPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -20,34 +26,66 @@ import cn.reactnative.modules.update.UpdatePackage;
 
 public class MainApplication extends Application implements ReactApplication {
 
-  private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
-    @Override
-    protected boolean getUseDeveloperSupport() {
-      return BuildConfig.DEBUG;
-    }
+    private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
+        @Override
+        protected boolean getUseDeveloperSupport() {
+            return BuildConfig.DEBUG;
+        }
 
 //    @Override
 //    protected String getJSBundleFile() {
 //      return UpdateContext.getBundleUrl(MainApplication.this);
 //    }
 
-    @Override
-    protected List<ReactPackage> getPackages() {
-      return Arrays.<ReactPackage>asList(
-          new MainReactPackage(),
-          new ReactNativeDialogsPackage(),
-          new BlurViewPackage(),
-          new OrientationPackage(),
-          new VectorIconsPackage(),
-          new RNDeviceInfo(),
-          new UpdatePackage(),
-          new ImagePickerPackage()
-      );
-    }
-  };
+        @Override
+        protected List<ReactPackage> getPackages() {
+            return Arrays.<ReactPackage>asList(
+                    new MainReactPackage(),
+                    new BaiChuanPackage(),
+                    new ReactNativeDialogsPackage(),
+                    new BlurViewPackage(),
+                    new OrientationPackage(),
+                    new VectorIconsPackage(),
+                    new RNDeviceInfo(),
+                    new UpdatePackage(),
+                    new ImagePickerPackage()
+            );
+        }
+    };
 
-  @Override
-  public ReactNativeHost getReactNativeHost() {
-      return mReactNativeHost;
-  }
+    @Override
+    public ReactNativeHost getReactNativeHost() {
+        return mReactNativeHost;
+    }
+
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        SoLoader.init(this, /* native exopackage */ false);
+        // 初始化参数依次为 this, AppId, AppKey
+//        AVOSCloud.initialize(this, "q81jdsbi5qp679fi5o46i5nppjgycztgivwj30707xfvehzt",
+//                "y6ffzv6mq705pya2pd6kgl1ni1vwlppesis7f1qi19afg5nn");
+
+//        AlibcTradeSDK.setForceH5(TRUE);
+        AlibcTradeSDK.asyncInit(this, new AlibcTradeInitCallback() {
+            @Override
+            public void onSuccess() {
+                //初始化成功，设置相关的全局配置参数
+                // ...
+                Log.d("1","百川配置成功");
+                AlibcTaokeParams taokeParams = new AlibcTaokeParams("mm_48051893_0_0","","");
+                AlibcTradeSDK.setTaokeParams(taokeParams);
+                AlibcTradeSDK.setForceH5(true);
+            }
+
+            @Override
+            public void onFailure(int code, String msg) {
+                //初始化失败，可以根据code和msg判断失败原因，详情参见错误说明
+                Log.e("百川配置"+ code,msg);
+            }
+        });
+
+//        AlibabaAuth
+    }
 }
